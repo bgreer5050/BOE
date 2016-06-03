0dcc18562aff45f494ea071fddbebc14d9bb1b43
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-06-03 06:39:56 -0500

    Add project files.

---
+++ BOE.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOE.Domain
+{
+    public class Class1
+    {
+    }
+}
